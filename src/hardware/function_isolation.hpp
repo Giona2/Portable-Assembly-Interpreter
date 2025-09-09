e99088e45dfb9e9bcb09b92c9e16195c6742c4c4
Jonah Beston <jonah.beston@gmail.com>
2025-09-09 12:28:25 -0400

    Improved function isolation

--- src/hardware/function_isolation.hpp
+++ src/hardware/function_isolation.hpp
@@ -1,2 +1,18 @@
-void init_while() {
+#include "supported_platforms.hpp"
+
+
+__attribute__((always_inline)) inline void init_while() {
+    #ifdef LINUX_AMD_64
+    __asm__ volatile (
+        "push %rax\n"   // Use %%rax instead of %rax
+    );
+    #endif
+}
+
+__attribute__((always_inline)) inline void end_while() {
+    #ifdef LINUX_AMD_64
+    __asm__ volatile (
+        "pop %rax\n"   // Use %%rax instead of %rax
+    );
+    #endif
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "stdcwc/fs.hpp"
 #include "stdcwc/error.hpp"
-#include <cstdio>
+#include "hardware/function_isolation.hpp"
 #include <stdint.h>
 using namespace stdcwc;
 
@@ -10,9 +10,14 @@ int main() {
 	Vec<u_int8_t> file_content = file.read();
 	volatile u_int8_t *current_byte_instruction = file_content.inner;
 
+	init_while();
 	while (*current_byte_instruction != 0xFF) {
+		end_while();
+
+		init_while();
 		current_byte_instruction += 1;
 	}
+	end_while();
 
 	file.close();
 }
