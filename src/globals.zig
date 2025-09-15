088b9089847bfbc25adb6b3750c02abb577dda3a
Jonah Beston <jonah.beston@gmail.com>
2025-09-14 22:02:54 -0400

    Updated globals to reflect the stack overhaul

--- src/globals.zig
+++ src/globals.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+const stdzig = @import("stdzig/stdzig.zig");
+    const collections = stdzig.collections;
+        const Vec = collections.Vec;
 
 
 // ===========================
@@ -8,38 +11,27 @@ export var current_byte_address: usize = 0;
 // ===========================
 
 
-// =========================
-// === Variable Handling ===
-// =========================
-export var variable_buffer = VariableBuffer.init();
+// ======================
+// === Stack Handling ===
+// ======================
+const maximum_variable_count: comptime_int = 32;
 
-const max_variable_count: comptime_int = 50;
+export var current_variable_frame: CurrentVariableFrame = undefined;
 
+export var caller_variable_frames: CallerVariableFrames = undefined;
 
-pub const Variable = extern struct {
-    is_active: bool,
-    address: usize,
-    size: i8,
+const CallerVariableFrames = extern struct {
+    inner: Vec(Vec(Variable)),
 };
 
-pub const VariableBuffer = extern struct {
-    buffer: [max_variable_count]Variable,
+const CurrentVariableFrame = extern struct {
+    inner: [maximum_variable_count]Variable = [_]Variable{ .{.is_active = false, .size = 0}**maximum_variable_count },
+};
 
-    pub fn init() VariableBuffer {
-        return .{ .buffer = [_]Variable{.{.is_active = false, .address = 0, .size = 0}}**max_variable_count };
-    }
-
-    pub noinline fn add_variable(self: *VariableBuffer, size: i8, address: usize) void {
-        // Find the next available space
-        var variable_index: usize = 0;
-        while (variable_index < self.buffer.len) {
-            if (!self.buffer[variable_index].is_active) break;
-            variable_index += 1;
-        }
-
-        // Construct the variable
-        self.buffer[variable_index] = .{ .is_active = true, .address = address, .size = size };
-    }
+const Variable = extern struct {
+    is_active: bool,
+    size: i8,
+    value: usize,
 };
 // =========================
 
