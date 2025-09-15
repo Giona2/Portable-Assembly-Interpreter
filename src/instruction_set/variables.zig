5fe4d046b322d462fb3423aed3332aa4f048d6f8
Jonah Beston <jonah.beston@gmail.com>
2025-09-14 21:35:16 -0400

    Holy shit. I know what we're going to do today Finneas
    Instead of relying on the physical stack, I'll instead have the
    following layout for function calling: The current running function's
    stack is managed by a static list in .data that holds, let's say, 1024
    bytes. When the caller invokes a callee and the callee creates a new
    stack frame, it pushes the caller's frame onto the heap and clear out
    the curent stack frame for the callee

--- src/instruction_set/variables.zig
+++ src/instruction_set/variables.zig
@@ -24,31 +24,9 @@ pub inline fn exec_stt() void {
 }
 
 pub inline fn exec_new() void {
-    // Move to the byte size
-    current_byte_address += 1;
-
-    // Allocate space on the stack
-    stack.alloc_stack(stack_slot_size);
-
-    // Register the new variable
-    variable_buffer.add_variable(@sizeOf(default_variable_size), asm volatile("" : [ret] "={rsp}" (->usize)));
 }
 
 pub inline fn exec_set() void {
-    // Move to the variable index
-    current_byte_address += 1;
-
-    // Set the variable index
-    asm volatile (
-        \\ movq %[value], (%[variable_address])
-        :
-        : [variable_address] "r" ( variable_buffer.buffer[@as(usize, (@as(*u8, @ptrFromInt(current_byte_address))).*)].address ),
-          [value]            "r" ( @as(*default_variable_size, @ptrFromInt(current_byte_address+1)).* ),
-        : "memory"
-    );
-
-    // Move to the end of this instruction
-    current_byte_address += @sizeOf(default_variable_size);
 }
 
 pub inline fn exec_lod() void {
